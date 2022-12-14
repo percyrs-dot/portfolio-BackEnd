80d9db38e300c656f5ce4426922157d9dd8f2994
percy <percyers@outlook.com>
2022-12-14 15:03:11 -0300

    JWT auth with in memory credentials

--- src/main/java/com/portfolio/BackEnd/config/WebConfig.java
+++ src/main/java/com/portfolio/BackEnd/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.portfolio.BackEnd;
+package com.portfolio.BackEnd.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
