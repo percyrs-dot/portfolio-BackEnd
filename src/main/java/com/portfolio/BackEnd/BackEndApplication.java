1f1b7ee5ee27d983ce26d0bd1cf25ee7490f0dda
percy <percyers@outlook.com>
2022-12-20 19:12:57 -0300

    fix 2

--- src/main/java/com/portfolio/BackEnd/BackEndApplication.java
+++ src/main/java/com/portfolio/BackEnd/BackEndApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class BackEndApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
